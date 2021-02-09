f1cab8eeccd088b13bd51a3917eac9efe953d963
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2021-02-09 14:48:14 +0200

    1st ver. of lab 13

---
+++ src/FormOfEnroll.java
@@ -0,0 +1,6 @@
+public enum FormOfEnroll {
+    BUDGET,
+    CONTRACT
+}
+
+
---
+++ src/SameFormOfEnrollException.java
@@ -0,0 +1,15 @@
+public class SameFormOfEnrollException extends Exception{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public SameFormOfEnrollException(String message, Student student) {
+        super(message);
+        name = student.getName();
+    }
+
+
+}
